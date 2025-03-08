dac184ec38c8026977b036a27caa6ef4f1252efb
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-03-08 11:01:17 +0600

    Added Buttons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+// SlickSlider
 $(".Sliders").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  dots: true,
   // arrows: true,
   prevArrow: '<i class="fa-solid fa-chevron-left prev"></i>',
   nextArrow: '<i class="fa-solid fa-chevron-right next"></i>',
 });
+// SlickSlider
+
+// Back to Top
+//==== Back-to-top button
+$(window).on('scroll', function(event) {
+  if($(this).scrollTop() >100){
+      $('.bactoTop').fadeIn(200)
+  } else{
+      $('.bactoTop').fadeOut(200)
+  }
+});
+//==== Animate the scroll to top
+$('.bactoTop').on('click', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: 0,
+  }, 100);
+});
+// Back to Top
