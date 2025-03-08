d8e121ebf415809efbdb3b76a950af1fda1428b2
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-03-08 09:37:54 +0600

    Added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".Sliders").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  // arrows: true,
+  prevArrow: '<i class="fa-solid fa-chevron-left prev"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right next"></i>',
+});
